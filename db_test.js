0e41f41f72ee62a8ccccdbfbfc422a587f4dfe04
Janine Healy <janine.f.healy@gmail.com>
2021-03-17 16:56:43 +0000

    Created db_test.js

--- db_test.js
+++ db_test.js
@@ -1,32 +1,25 @@
-// Import MySQL library
-const mysql = require('mysql');
+// Import SQLite library.
+// Use verbose mode to give more detailed error outputs
+const sqlite3 = require("sqlite3").verbose();
 
 // Connect to the database.
-// Assumes there is a root user with no password.
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "schoolDB"
-});
-
 // Function is callback when connection completed.
 // err is any error message that occurs
-con.connect(function(err) {
+let db = new sqlite3.Database("school2.db", function(err) {
     // If an error, print it out.
     if (err) {
         return console.error(err.message);
     }
-    console.log("Connected to students database.");
+    console.log("Connected to School database.");
 });
 
 // Close the database connection.
 // Always close the connection when you are finished with it.
 // Function is callback when connection is closed.
-con.end(function(err) {
+db.close(function(err) {
     // If an error, print it out.
     if (err) {
         return console.error(err.message);
     }
-    console.log("Closed connection to students database.");
+    console.log("Closed connection to School database.");
 });
