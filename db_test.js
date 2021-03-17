708cf0d0a5b86b07a026c21118efaf48358d7a6d
Janine Healy <janine.f.healy@gmail.com>
2021-03-17 13:28:28 +0000

    db_test and server.js test

---
+++ db_test.js
@@ -0,0 +1,32 @@
+// Import MySQL library
+const mysql = require('mysql');
+
+// Connect to the database.
+// Assumes there is a root user with no password.
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "students"
+});
+
+// Function is callback when connection completed.
+// err is any error message that occurs
+con.connect(function(err) {
+    // If an error, print it out.
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Connected to students database.");
+});
+
+// Close the database connection.
+// Always close the connection when you are finished with it.
+// Function is callback when connection is closed.
+con.end(function(err) {
+    // If an error, print it out.
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log("Closed connection to students database.");
+});
