e757790c4e61cbfaa7c61c072d4db2e5ce10b5f2
Shoaib Iqbal <shoaibiqbal@gmail.com>
2021-03-20 20:53:31 +0000

    Connected Database

--- data/data.js
+++ data/data.js
@@ -1,2 +1,18 @@
+// Use Strict Mode
 "use strict";
 
+// Import sqlite3
+const sqlite3 = require("sqlite3").verbose();
+
+// Import student class file
+const student = require("../student");
+
+// Create new database instance
+var db = new sqlite3.Database("school2.db", function(err){
+    // If there is any error
+    if (err) {
+        console.log(err.message);
+    }
+    // Connection message
+    console.log("Connected to Database");
+});
