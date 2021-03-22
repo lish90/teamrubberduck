a8c034ec7e01b00c7ece92fd0493e43181ca431c
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2021-03-22 19:53:05 +0000

    Created end points and working search buttons on pages

--- data/data.js
+++ data/data.js
@@ -17,6 +17,7 @@ var db = new sqlite3.Database("data/school2.db", function(err){
     console.log("Connected to Database");
 });
 
+// create the function which extracts data from database and makes a new Student instance
 exports.getStudent = function(full_name, callback){
     var sql = `SELECT * FROM Student WHERE Student_Name="${full_name}"`;
     db.get(sql, function(err, row){
@@ -27,3 +28,28 @@ exports.getStudent = function(full_name, callback){
         callback(newStudent);
     });
 }
+
+// create the function which extracts data from database and makes a new Teacher instance
+exports.getTeacher = function(teacher_name, callback){
+    var sql = `SELECT * FROM Teacher WHERE Teacher_Name="${teacher_name}"`;
+    db.get(sql, function(err, row){
+        if (err){
+            console.log(err.message);
+        }
+        var newTeacher = new student.Teacher(row.Teacher_ID, row.Teacher_Name, row.Subject, row.Years_Served, row.Salary, row.Street_Address, row.Postal_Code, row.Contact_Number);
+        callback(newTeacher);
+    });
+}
+
+// create the function which extracts data from database and makes a new Timetable instance
+exports.getTimetable = function(subject, callback){
+    var sql = `SELECT * FROM Timetable WHERE Subject="${subject}"`;
+    db.get(sql, function(err, row){
+        if (err){
+            console.log(err.message);
+        }
+        var newTimetable = new student.Timetable(row.Class_ID, row.Teacher_ID, row.Subject, row.Room_Number, row.Day, row.Time);
+        callback(newTimetable);
+    });
+}
+
--- static/student_information.js
+++ static/student_information.js
@@ -1,8 +1,11 @@
 "use strict";
 
+// Creating an angular app
 var mainApp = angular.module("mainApp", []);
 
+// Pointing out the controller for mainApp
 mainApp.controller("studentController", function($scope, $http){
+    // Create studentSearchButton which will get the data from /student/<student name>
     $scope.studentSearchButton = function(){
         $http.get("/student/" + $scope.studentSearchBox).then(function(response){
             $scope.selectedStudent = response.data;
